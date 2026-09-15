24610fe7040c6c5ce3720742d09ce5c34abbc52e
Elizaveta52 <e.s.krukova@yandex.ru>
2026-09-15 15:14:26 +0300

    Добавлен ввод для трубы

--- KrukovaLR1/KrukovaLR1.cpp
+++ KrukovaLR1/KrukovaLR1.cpp
@@ -10,10 +10,21 @@ struct Pipe
     int diameter;
     bool repair;
 };
+void Add_Pipe(Pipe& p)
+{
+    cout << "Enter pipe name: ";
+    cin >> p.name;
+    cout << "Enter pipe length (km): ";
+    cin >> p.length;
+    cout << "Enter pipe diameter (mm): ";
+    cin >> p.diameter;
+    p.repair = false;
+}
 
 int main()
 {
-    cout << "Pipeline Transport System" << endl;
+    Pipe p;
+    Add_Pipe(p);
     return 0;
 }
 
